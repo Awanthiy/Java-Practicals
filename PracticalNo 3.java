aa26d559abdefabca5e481c1fa9c9ff7e078c6c9
Awanthi Malawanage <121420903+Awanthiy@users.noreply.github.com>
2023-07-01 16:25:35 +0530

    PracticalNo 3.java

--- PracticalNo 3.java
+++ PracticalNo 3.java
@@ -78,4 +78,60 @@ public class Excer31
         Employee employee = new Employee("John Doe", 30, 100000);
     }
 }
+//2.
+class Employee {
+    private String name;
+    private int basicSalary;
+    private int bonus;
+
+    public Employee(String name, int basicSalary, int bonus) {
+        this.name = name;
+        this.basicSalary = basicSalary;
+        this.bonus = bonus;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBasicSalary() {
+        return basicSalary;
+    }
+
+    public void setBasicSalary(int basicSalary) {
+        this.basicSalary = basicSalary;
+    }
+
+    public int getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(int bonus) {
+        this.bonus = bonus;
+    }
+
+    public int calculateBonusAmount() {
+        return this.basicSalary + this.bonus;
+    }
+
+    public void printDetails() {
+        System.out.println("Employee Name: " + this.name);
+        System.out.println("Basic Salary: " + this.basicSalary);
+        System.out.println("Bonus: " + this.bonus);
+        System.out.println("Bonus Amount: " + this.calculateBonusAmount());
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Employee employee = new Employee("Bogdan", 50000, 10000);
+
+        employee.printDetails();
+    }
+}
+
 
